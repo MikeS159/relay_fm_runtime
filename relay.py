a3393104d136b8b023519eb8adf755b0b6e70457
MikeS159 <mike@ns334377.ip-5-196-65.eu>
2020-01-30 20:28:56 +0100

    fixed title

--- relay.py
+++ relay.py
@@ -131,7 +131,7 @@ def main():
     else:
         file = open("docs/index.md","w")
 
-    file.write("Title: Relay FM Stats")
+    file.write("Title: Relay FM Stats\n\n")
 
     for s in summary_output:
         file.write(s)
