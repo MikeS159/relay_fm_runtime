1b8291a064257b755ae5be5f53fcee66d627c213
Mike Simpson <mike.simpson@phabrix.com>
2020-03-04 13:17:35 +0000

    Code to check for new shows that need to be added. Also some place holder code to check if graphs should be updated (once they are added)

--- relay.py
+++ relay.py
@@ -6,9 +6,8 @@ import sys
 from git import Repo
 from datetime import datetime
 
-shows_list = ['adapt', 'analogue', 'automators', 'bonanza', 'b-sides', 'clockwise', 'connected', 'cortex', 'departures', 'flashback', 'focused', 'liftoff', 'mpu', 'makedo', 'material', 'originality', 'parallel', 'pictorial', 'presentable', 'rd', 'remaster', 'roboism', 'rocket', 'penaddict', 'testdrivers', 'tc', 'topfour', 'radar', 'ungeniused', 'upgrade']
-
-old_shows_list = ['almanac', 'bionic', 'canvas', 'cmdspace', 'disruption', 'download', 'inquisitive', 'isometric', 'ltoe', 'mixedfeelings', 'playingforfun', 'query', 'subnet', 'prompt', 'virtual']
+shows_list = []
+old_shows_list = []
 
 h2 = "## "
 h3 = "### "
@@ -23,6 +22,7 @@ show_output = []
 old_show_output = []
 summary_output = []
 new_show_output = []
+shows_Checked = []
 
 intervals = (
     ('years', 31536000),
@@ -32,6 +32,11 @@ intervals = (
     ('mins', 60),
     ('secs', 1),
     )
+
+class Show:
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+
 
 def display_time(seconds, granularity=2):
     result = []
@@ -62,7 +67,7 @@ def parse_feed(feed_name):
     old_show_output.append(display_time(avg,4) + "|\n")
     return total_len
 
-def parse_prediction_feed(feed_name):
+def parse_prediction_feed(feed_name, last_checked):
     base_feed = 'https://www.relay.fm/'
     end_feed = '/feed'
     total_feed = base_feed + feed_name + end_feed
@@ -71,6 +76,18 @@ def parse_prediction_feed(feed_name):
     total_len = 0
     num_shows = len(ents)
     time_list = []
+
+    update_needed = False
+    s = ents[0]['id']
+    s = s.replace("http://relay.fm/", "")
+    ss = s.split('/')
+    if(len(ss) == 2):
+        show_Checked = Show(name=ss[0], episode=ss[1])
+        shows_Checked.append(show_Checked)
+        if int(ss[1]) > int(last_checked):
+            update_needed = True
+            print("Update Needed")
+    
     for e in ents:
         length = e['itunes_duration']
         total_len += int(length)
@@ -97,20 +114,77 @@ def parse_prediction_feed(feed_name):
     return total_len, yearly_output
 
 
+def getShows():
+    master_Feed = "https://www.relay.fm/master/feed"
+    d = feedparser.parse(master_Feed)    
+    ents = d['entries']
+    newest_Shows = []
+    names = []
+    for e in ents:
+        s = e['id']
+        s = s.replace("http://relay.fm/", "")
+        ss = s.split('/')
+        if(len(ss) == 2):
+            showToAdd = Show(name=ss[0], episode=ss[1])
+            # Shows are newest first, so duplicates won't be added
+            if(ss[0] not in names):
+                newest_Shows.append(showToAdd)
+            names.append(ss[0])
+    return newest_Shows
+
+def readShowList(path):
+    old_shows = []
+    current_shows = []
+    file = open(path + "oldShows.txt", "r")
+    for f in file:
+        old_shows.append(f.rstrip())
+    file = open(path + "currentShows.txt", "r")
+    for f in file:
+        showInfo = f.rstrip().split(":")        
+        if(len(showInfo) == 2):
+            showToAdd = Show(name=showInfo[0], lastCheckedEpisode=showInfo[1])            
+            current_shows.append(showToAdd)            
+    return old_shows, current_shows      
+
+def compareShows(latest_shows, shows_list):
+    needs_update = []
+    new_shows = []
+    for latest in latest_shows:
+        showName = latest.name
+        found = False
+        last_Episode = -1
+        for show in shows_list:
+            if show.name == showName:
+                found = True
+                last_Episode = show.lastCheckedEpisode
+                break
+        if found:
+            if int(last_Episode) < int(latest.episode):
+                needs_update.append(showName)
+        else:
+            new_shows.append(showName)
+    return needs_update, new_shows
+
 def main():
     now = datetime.now()
     use_git = False
+    path = ""
     if len(sys.argv) > 1:
         print("Using Git")
         use_git = True
-        path = sys.argv[1]
+        path = sys.argv[1] + "/"
         git_repo = Repo(path)
         git_repo.git.pull()
 
+    old_shows_list, shows_list = readShowList(path)
+    latest_shows = getShows()
+    shows_to_update, new_shows = compareShows(latest_shows, shows_list)
+
+    #sys.exit(0)
     running_total = 0
     yearly_output = 0
     for show in shows_list:
-        total, yearly = parse_prediction_feed(show)
+        total, yearly = parse_prediction_feed(show.name, show.lastCheckedEpisode)
         running_total += total
         yearly_output += yearly
     for show in old_shows_list:
@@ -128,8 +202,7 @@ def main():
 
     file = 0
     if use_git:
-
-        file = open(path + "/docs/index.md","w")
+        file = open(path + "docs/index.md","w")
     else:
         file = open("docs/index.md","w")
 
@@ -150,9 +223,21 @@ def main():
     for s in old_show_output:
         file.write(s)
 
+    for new_show in new_shows:
+        file.write("\nNew show needs adding - " + new_shows)
+    
     current_time = now.strftime("%H:%M:%S %d/%m/%Y")
     file.write("\nGenerated at: " + current_time + "\n")
 
+    file.close()
+
+    if use_git:
+        file = open(path + "currentShows.txt","w")
+    else:
+        file = open("currentShows.txt","w")
+    for checked in shows_Checked:
+        s = str(checked.name) + ":" + str(checked.episode) + "\n"
+        file.write(s)
     file.close()
 
     if use_git:
